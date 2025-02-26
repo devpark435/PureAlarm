475f5b8ed79b29a4fca54630147a7802b0f994b6
devpark435 <devpark435@gmail.com>
2025-02-26 21:45:26 +0900

    feat #2 - 알람 제목 커스텀 텍스트 필드 제작

--- PureAlarm/Common/Components/CustomTextField.swift
+++ PureAlarm/Common/Components/CustomTextField.swift
@@ -5,4 +5,50 @@
 //  Created by 박현렬 on 2/26/25.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+import Then
+
+// MARK: - 커스텀 텍스트 필드
+class CustomTextField: UITextField {
+    
+    private let padding = UIEdgeInsets(top: 0, left: 15, bottom: 0, right: 15)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupTextField()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupTextField()
+    }
+    
+    private func setupTextField() {
+        backgroundColor = UIColor(white: 0.2, alpha: 1.0)
+        layer.cornerRadius = 10
+        textColor = .white
+        tintColor = UIColor(red: 0.4, green: 0.6, blue: 1.0, alpha: 1.0)
+        
+        // 플레이스홀더 스타일
+        attributedPlaceholder = NSAttributedString(
+            string: placeholder ?? "",
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor(white: 0.7, alpha: 1.0)]
+        )
+        
+        // 클리어 버튼 스타일
+        clearButtonMode = .whileEditing
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+    
+    override func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+}
