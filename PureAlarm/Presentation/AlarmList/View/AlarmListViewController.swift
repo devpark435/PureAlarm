697171c2dc49975ef70baee4dcdf9bbc72f98906
devpark435 <devpark435@gmail.com>
2025-02-26 20:24:19 +0900

    setting #1 - project 초기 설정

---
+++ PureAlarm/Presentation/AlarmList/View/AlarmListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PureAlarm
+//
+//  Created by 박현렬 on 2/26/25.
+//
+
+import UIKit
+
+class AlarmListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
